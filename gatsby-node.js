111e51bcba08055184ec780e5ab94433fc3fb940
jjanmo <jjanmo07@hanmail.net>
2022-03-05 00:06:19 +0900

    fix: update path setting for fixing path error

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,8 +18,9 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     resolve: {
       alias: {
         components: path.resolve(__dirname, 'src/components'),
-        utils: path.resolve(__dirname, 'src/utils'),
-        hooks: path.resolve(__dirname, 'src/hooks'),
+        constants: path.resolve(__dirname, 'src/constants'),
+        assets: path.resolve(__dirname, 'src/assets'),
+        styles: path.resolve(__dirname, 'src/styles'),
       },
     },
   })
