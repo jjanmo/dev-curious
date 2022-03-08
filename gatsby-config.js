2f591d727f774337911e3eca65b2a2f7253f07e0
jjanmo <jjanmo07@hanmail.net>
2022-03-08 20:04:52 +0900

    feat: setup some library to use markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,36 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
+    {
+      resolve: 'gatsby-remark-smartypants',
+      options: {
+        dashes: 'oldschool',
+      },
+    },
+    {
+      resolve: 'gatsby-remark-prismjs',
+      options: {
+        classPrefix: 'language-',
+      },
+    },
+    {
+      resolve: 'gatsby-remark-images',
+      options: {
+        maxWidth: 768,
+        quality: 100,
+        withWebp: true,
+      },
+    },
+    {
+      resolve: 'gatsby-remark-copy-linked-files',
+      options: {},
+    },
+    {
+      resolve: 'gatsby-remark-external-links',
+      options: {
+        target: '_blank',
+        rel: 'nofollow',
+      },
+    },
   ],
 }
