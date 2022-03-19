4d577c35ee99fa055fb37b819aa46108753cfbf3
jjanmo <jjanmo07@hanmail.net>
2022-03-19 18:09:13 +0900

    feat: add nav component

---
+++ src/components/Nav/styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 40rem;
+
+  list-style: none;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+`
