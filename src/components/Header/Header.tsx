e19ee718fee4b897f95b9325baa556c337a79601
jjanmo <jjanmo07@hanmail.net>
2022-03-19 18:08:46 +0900

    refactor: seperate styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,7 @@
-import styled from '@emotion/styled'
-import { COLORS } from 'constants/colors'
+import { Container, Logo } from './styles'
+import Nav from 'components/Nav'
 import { Link } from 'gatsby'
 import { useEffect, useState } from 'react'
-
-const Container = styled.header<{ isScrolling: boolean }>`
-  position: fixed;
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: ${({ isScrolling }) => (isScrolling ? '7.5rem' : '5rem')};
-
-  background-color: ${COLORS.MAIN_BLUE};
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  z-index: 100;
-  transition: all 0.2s ease-in-out;
-  opacity: ${({ isScrolling }) => (isScrolling ? 0.85 : 1)};
-`
-const Logo = styled.div<{ isScrolling: boolean }>`
-  display: flex;
-  font-weight: bold;
-  position: relative;
-  margin: 0 10rem;
-
-  & > div:first-of-type {
-    position: absolute;
-    top: ${({ isScrolling }) => (isScrolling ? '50px' : '0px')};
-    left: -40px;
-    color: ${COLORS.MAIN_YELLOW};
-    font-family: 'Roboto', sans-serif;
-    font-size: 1.5rem;
-    transition: all 0.4s ease-in-out;
-  }
-  & > div:last-of-type {
-    color: ${COLORS.SUB_YELLOW};
-    font-family: 'Hubballi', cursive;
-    font-size: 3rem;
-  }
-  & > div {
-    position: relative;
-  }
-`
 
 const Header = () => {
   const [isScrolling, setIsScrolling] = useState(false)
@@ -69,6 +26,14 @@ const Header = () => {
           <div>Curious</div>
         </Logo>
       </Link>
+      <Nav />
+      <div>
+        <input />
+      </div>
+      <div>
+        <input type="radio" />
+        <input type="radio" />
+      </div>
     </Container>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled'
+import { COLORS } from 'constants/colors'
+
+export const Container = styled.header<{ isScrolling: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: ${({ isScrolling }) => (isScrolling ? '7.5rem' : '5rem')};
+
+  background-color: ${COLORS.MAIN_BLUE};
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 100;
+  transition: all 0.2s ease-in-out;
+  opacity: ${({ isScrolling }) => (isScrolling ? 0.85 : 1)};
+`
+export const Logo = styled.div<{ isScrolling: boolean }>`
+  display: flex;
+  font-weight: bold;
+  position: relative;
+  margin: 0 10rem;
+
+  & > div:first-of-type {
+    position: absolute;
+    top: ${({ isScrolling }) => (isScrolling ? '50px' : '0px')};
+    left: -40px;
+    color: ${COLORS.MAIN_YELLOW};
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.5rem;
+    transition: all 0.4s ease-in-out;
+  }
+  & > div:last-of-type {
+    color: ${COLORS.SUB_YELLOW};
+    font-family: 'Hubballi', cursive;
+    font-size: 3rem;
+  }
+  & > div {
+    position: relative;
+  }
+`
