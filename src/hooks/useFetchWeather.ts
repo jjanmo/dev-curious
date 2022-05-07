30d346d7f1d76b635e42b785cf18c7963fceb6ee
jjanmo <jjanmo07@hanmail.net>
2022-05-07 21:32:29 +0900

    feat: fetch: weather

---
+++ src/hooks/useFetchWeather.ts
@@ -0,0 +1,46 @@
+// import useSWR from "swr"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import useSWR, { Fetcher } from 'swr'
+import { WeatherType } from 'types/weather'
+
+interface LocationType {
+  latitude: number
+  longitude: number
+}
+interface WeatherResponseType {
+  data: WeatherType
+}
+
+function useFetchWeather<T>() {
+  const [location, setLocation] = useState<LocationType | null>()
+
+  const fetcher = () =>
+    axios
+      .get<T>(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `https://api.weatherapi.com/v1/current.json?key=${process.env.GATSBY_WEATHER_API_KEY}&q=${location?.latitude},${location?.longitude}`
+      )
+      .then(res => res.data)
+
+  const { data } = useSWR<T>(location ? 'current-weather' : null, fetcher)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const {
+        coords: { latitude, longitude },
+      } = position
+      console.log(latitude, longitude)
+      setLocation({
+        latitude,
+        longitude,
+      })
+    })
+  }, [])
+
+  return {
+    data,
+  }
+}
+
+export default useFetchWeather
