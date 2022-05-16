7e7aaeedbcd31f3686e5c84a94bef02b658a9f98
jjanmo <jjanmo07@hanmail.net>
2022-05-16 09:11:36 +0900

    fix: fix navigation

--- src/components/Navigation/Navigation.style.tsx
+++ src/components/Navigation/Navigation.style.tsx
@@ -1,16 +1,22 @@
 import styled from '@emotion/styled'
+import { COLORS } from 'constants/colors'
 
 export const Container = styled.ul`
   display: flex;
   justify-content: center;
   align-items: center;
   list-style: none;
+
+  max-width: 76.8rem;
+  margin: 5rem auto;
 `
-export const Item = styled.li`
+export const Item = styled.li<{ current: boolean }>`
   width: 15rem;
   padding: 1rem 1.5rem;
-  border-radius: 3rem;
   transition: all 0.4s ease-in-out;
+  color: ${({ current }) => (current ? COLORS.GREEN_020 : COLORS.BLACK)};
+  border-bottom: ${({ current }) => (current ? `solid 2px ${COLORS.GREEN_020}` : `none`)};
+
   & a {
     width: 100%;
     height: 100%;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,23 @@
 import * as S from './Navigation.style'
 import { Link } from 'gatsby'
+import type { PageProps } from 'gatsby'
 
-function Navigation() {
+function Navigation(props: PageProps) {
+  const { pageResources } = props
+
+  console.log(props.path)
+
   return (
     <S.Container>
-      <S.Item>
+      {/* <S.Item current={path === '/'}>
         <Link to="/">글</Link>
       </S.Item>
-      <S.Item>
-        <Link to="/tags">시리즈</Link>
+      <S.Item current={path === '/series'}>
+        <Link to="/series">시리즈</Link>
       </S.Item>
-      <S.Item>
+      <S.Item current={path === '/about'}>
         <Link to="/about">소개</Link>
-      </S.Item>
+      </S.Item> */}
     </S.Container>
   )
 }
