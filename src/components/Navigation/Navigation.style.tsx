3553c8afc71552320d6553e3260ded8caef9698b
jjanmo <jjanmo07@hanmail.net>
2022-05-19 23:44:50 +0900

    feat: add active style in navigation

--- src/components/Navigation/Navigation.style.tsx
+++ src/components/Navigation/Navigation.style.tsx
@@ -6,26 +6,30 @@ export const Container = styled.ul`
   justify-content: center;
   align-items: center;
   list-style: none;
-
   max-width: 76.8rem;
   margin: 5rem auto;
 `
 export const Item = styled.li<{ current?: boolean }>`
   width: 15rem;
-  padding: 1rem 1.5rem;
-  transition: all 0.4s ease-in-out;
-  color: ${({ current }) => (current ? COLORS.GREEN_020 : COLORS.BLACK)};
-  border-bottom: ${({ current }) => (current ? `solid 2px ${COLORS.GREEN_020}` : `none`)};
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   & a {
+    display: block;
     width: 100%;
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     font-weight: 600;
     font-size: 2.2rem;
     letter-spacing: 0.5px;
     text-transform: uppercase;
+    text-align: center;
+    color: ${COLORS.GRAY_040};
+
+    &.active {
+      color: ${COLORS.GREEN_020};
+      border-bottom: solid 2px ${COLORS.GREEN_020};
+    }
   }
 `
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,27 @@
 import * as S from './Navigation.style'
 import { Link } from 'gatsby'
-import type { PageProps } from 'gatsby'
 
 function Navigation() {
+  const activateLink = ({ isCurrent }: { isCurrent: boolean }) => {
+    return isCurrent ? { className: 'active' } : {}
+  }
+
   return (
     <S.Container>
       <S.Item>
-        <Link to="/">글</Link>
+        <Link to="/" getProps={activateLink}>
+          글
+        </Link>
       </S.Item>
       <S.Item>
-        <Link to="/series">시리즈</Link>
+        <Link to="/series" getProps={activateLink}>
+          시리즈
+        </Link>
       </S.Item>
       <S.Item>
-        <Link to="/about">소개</Link>
+        <Link to="/about" getProps={activateLink}>
+          소개
+        </Link>
       </S.Item>
     </S.Container>
   )
