f4492ff21841d7c3c6297a75807e976eba4b6b3a
jjanmo <jjanmo07@hanmail.net>
2022-05-21 19:51:46 +0900

    feat: change header ui

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import { COLORS } from 'constants/colors'
 import { Link } from 'gatsby'
@@ -40,4 +41,44 @@ export const SearchLink = styled(Link)`
   &:hover {
     background-color: ${COLORS.GRAY_010};
   }
+`
+export const ClipboardWrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+  .clipboard-button {
+    all: unset;
+  }
+`
+export const Copied = styled.span<{ active: boolean }>`
+  position: absolute;
+  bottom: -3rem;
+  left: 0rem;
+  display: ${({ active }) => (active ? 'inline-block' : 'none')};
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.GRAY_050};
+  opacity: 0.8;
+  font-size: 1.4rem;
+`
+export const MailText = styled.span<{ active: boolean }>`
+  position: absolute;
+  top: 0;
+  left: -16rem;
+  width: 0;
+  padding: 0;
+  font-size: 1.5rem;
+  border-radius: 5px;
+  opacity: 0;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.GRAY_050};
+  transition: all 0.3s ease-in-out;
+
+  ${({ active }) =>
+    active &&
+    css`
+      width: 16rem;
+      opacity: 0.9;
+      padding: 0.8rem 1rem;
+    `}
 `
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,36 @@
 import * as S from './Header.style'
-import { Logo } from 'components/Icons'
+import { Email, Github, Logo } from 'components/Icons'
 import { Link } from 'gatsby'
+import { useEffect, useState } from 'react'
+import Clipboard from 'react-clipboard.js'
 
 const Header = () => {
+  const [isHover, setIsHover] = useState({ github: false, mail: false })
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleMouseOver = (e: React.MouseEvent) => {
+    const target = e.currentTarget.id
+    setIsHover({
+      ...isHover,
+      [target]: true,
+    })
+  }
+  const handleMouseOut = (e: React.MouseEvent) => {
+    const target = e.currentTarget.id
+    setIsHover({
+      ...isHover,
+      [target]: false,
+    })
+  }
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false)
+      }, 1000)
+    }
+  }, [isCopied])
+
   return (
     <S.Container>
       <S.Logo>
@@ -12,10 +40,27 @@ const Header = () => {
         <S.LogoText>curious.note</S.LogoText>
       </S.Logo>
       <S.ButtonsContainer>
-        <div>다크모드 버튼</div>
-        {/* <S.SearchLink to="/search">
-          <Magnifier size={25} />
-        </S.SearchLink> */}
+        <Link
+          id="github"
+          to="https://github.com/jjanmo"
+          target="_blank"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          <Github size={32} isHover={isHover.github} />
+        </Link>
+        <S.ClipboardWrapper id="mail" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          <Clipboard
+            className="clipboard-button"
+            data-clipboard-text="jjanmo07@hanmail.net"
+            button-title="Copy to Clipboard"
+            onSuccess={() => setIsCopied(true)}
+          >
+            <Email width={52} height={38} isHover={isHover.mail} />
+          </Clipboard>
+          <S.Copied active={isCopied}>Copied!</S.Copied>
+          <S.MailText active={isHover.mail}>jjanmo07@hanmail.net</S.MailText>
+        </S.ClipboardWrapper>
       </S.ButtonsContainer>
     </S.Container>
   )
